adf096fbb871a7364216f5fd4a38cab2708a06b4
Chris Amori <chris.amori93@gmail.com>
2017-09-29 21:39:43 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,5 +2,4 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SampleContainer from './components/SampleContainer.jsx';
 
-console.log('we in dis vbich?')
 ReactDOM.render(<SampleContainer />, document.getElementById('app'));
