52a475df4ae36481edacf4767aca0e12e045c349
Chris Amori <chris.amori93@gmail.com>
2017-09-29 17:39:06 -0700

    Init commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import SampleContainer from './components/SampleContainer.jsx';
+
+console.log('we in dis vbich?')
+ReactDOM.render(<SampleContainer />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$|\.jsx$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  // Dev tools are provided by webpack
+  // Source maps help map errors to original react code
+  devtool: 'cheap-module-eval-source-map',
+
+  // Configuration for webpack-dev-server
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
+};
